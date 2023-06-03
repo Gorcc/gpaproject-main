a658176a1116f7f616fc99cbc2dbf13e1a0aa878
Deniz Görkem Ater <gorkem.ater1@hotmail.com>
2023-06-04 01:23:22 +0300

    add: admin

---
+++ src/Admin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Admin = () => {
+  return (
+    <div>
+      <h2>Logged in as Sysadmin</h2>
+    </div>
+  );
+};
+
+export default Admin;
--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Layout from "./Layout";
+
 import GPACalculator from "./GPACalculator";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from "./Admin";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route index element={<Layout />} />
+          <Route index element={<Admin />} />
           <Route path="Layout" element={<Layout />} />
           <Route path="homepage" element={<GPACalculator />} />
           <Route path="gpacalculator" element={<GPACalculator />} />
